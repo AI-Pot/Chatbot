2f910c45df3563f8f698958b7e750f6c6ff6d9ff
Jeongeun-Kwak <wjddms960921@gmail.com>
2021-05-23 22:50:32 +0900

    Add files via upload

---
+++ chatbot_v0.1.py
@@ -0,0 +1,578 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+
+# 포지셔널 인코딩 레이어
+class PositionalEncoding(tf.keras.layers.Layer):
+
+  def __init__(self, position, d_model):
+    super(PositionalEncoding, self).__init__()
+    self.pos_encoding = self.positional_encoding(position, d_model)
+
+  def get_angles(self, position, i, d_model):
+    angles = 1 / tf.pow(10000, (2 * (i // 2)) / tf.cast(d_model, tf.float32))
+    return position * angles
+
+  def positional_encoding(self, position, d_model):
+    angle_rads = self.get_angles(
+        position=tf.range(position, dtype=tf.float32)[:, tf.newaxis],
+        i=tf.range(d_model, dtype=tf.float32)[tf.newaxis, :],
+        d_model=d_model)
+    # 배열의 짝수 인덱스에는 sin 함수 적용
+    sines = tf.math.sin(angle_rads[:, 0::2])
+    # 배열의 홀수 인덱스에는 cosine 함수 적용
+    cosines = tf.math.cos(angle_rads[:, 1::2])
+
+    pos_encoding = tf.concat([sines, cosines], axis=-1)
+    pos_encoding = pos_encoding[tf.newaxis, ...]
+    return tf.cast(pos_encoding, tf.float32)
+
+  def call(self, inputs):
+    return inputs + self.pos_encoding[:, :tf.shape(inputs)[1], :]
+
+# 행렬 그리기
+#sample_pos_encoding = PositionalEncoding(50, 512)
+
+#plt.pcolormesh(sample_pos_encoding.pos_encoding.numpy()[0], cmap='RdBu')
+#plt.xlabel('Depth')
+#plt.xlim((0, 512))
+#plt.ylabel('Position')
+#plt.colorbar()
+#plt.show()
+
+# 스케일드 닷 프로덕트 어텐션 함수
+def scaled_dot_product_attention(query, key, value, mask):
+  """어텐션 가중치를 계산. """
+  matmul_qk = tf.matmul(query, key, transpose_b=True)
+
+  # scale matmul_qk
+  depth = tf.cast(tf.shape(key)[-1], tf.float32)
+  logits = matmul_qk / tf.math.sqrt(depth)
+
+  # add the mask to zero out padding tokens
+  if mask is not None:
+    logits += (mask * -1e9)
+
+  # softmax is normalized on the last axis (seq_len_k)
+  attention_weights = tf.nn.softmax(logits, axis=-1)
+
+  output = tf.matmul(attention_weights, value)
+
+  return output
+
+# 멀티 헤드 어텐션 구현하기
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, name="multi_head_attention"):
+        super(MultiHeadAttention, self).__init__(name=name)
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.query_dense = tf.keras.layers.Dense(units=d_model)
+        self.key_dense = tf.keras.layers.Dense(units=d_model)
+        self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+        self.dense = tf.keras.layers.Dense(units=d_model)
+
+    def split_heads(self, inputs, batch_size):
+        inputs = tf.reshape(
+            inputs, shape=(batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(inputs, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        query, key, value, mask = inputs['query'], inputs['key'], inputs['value'], inputs['mask']
+        batch_size = tf.shape(query)[0]
+
+        # linear layers
+        query = self.query_dense(query)
+        key = self.key_dense(key)
+        value = self.value_dense(value)
+
+        # 병렬 연산을 위한 머리를 여러 개 만듭니다.
+        query = self.split_heads(query, batch_size)
+        key = self.split_heads(key, batch_size)
+        value = self.split_heads(value, batch_size)
+
+        # 스케일드 닷 프로덕트 어텐션 함수
+        scaled_attention = scaled_dot_product_attention(query, key, value, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+        # 어텐션 연산 후에 각 결과를 다시 연결(concatenate)합니다.
+        concat_attention = tf.reshape(scaled_attention,
+                                  (batch_size, -1, self.d_model))
+
+         # final linear layer
+        outputs = self.dense(concat_attention)
+
+        return outputs
+
+# 패딩 마스킹
+def create_padding_mask(x):
+  mask = tf.cast(tf.math.equal(x, 0), tf.float32)
+  # (batch_size, 1, 1, sequence length)
+  return mask[:, tf.newaxis, tf.newaxis, :]
+
+# 룩 어헤드 마스크
+def create_look_ahead_mask(x):
+  seq_len = tf.shape(x)[1]
+  look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+  padding_mask = create_padding_mask(x)
+  return tf.maximum(look_ahead_mask, padding_mask)
+
+# 인코더 하나의 레이어를 함수로 구현.
+# 이 하나의 레이어 안에는 두 개의 서브 레이어가 존재합니다.
+def encoder_layer(units, d_model, num_heads, dropout, name="encoder_layer"):
+  inputs = tf.keras.Input(shape=(None, d_model), name="inputs")
+
+	# 패딩 마스크 사용
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+  # 첫 번째 서브 레이어 : 멀티 헤드 어텐션 수행 (셀프 어텐션)
+  attention = MultiHeadAttention(
+      d_model, num_heads, name="attention")({
+          'query': inputs,
+          'key': inputs,
+          'value': inputs,
+          'mask': padding_mask
+      })
+
+  # 어텐션의 결과는 Dropout과 Layer Normalization이라는 훈련을 돕는 테크닉을 수행
+  attention = tf.keras.layers.Dropout(rate=dropout)(attention)
+  attention = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(inputs + attention)
+
+  # 두 번째 서브 레이어 : 2개의 완전연결층
+  outputs = tf.keras.layers.Dense(units=units, activation='relu')(attention)
+  outputs = tf.keras.layers.Dense(units=d_model)(outputs)
+
+  # 완전연결층의 결과는 Dropout과 LayerNormalization이라는 훈련을 돕는 테크닉을 수행
+  outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+  outputs = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention + outputs)
+
+  return tf.keras.Model(
+      inputs=[inputs, padding_mask], outputs=outputs, name=name)  
+
+def encoder(vocab_size,
+            num_layers,
+            units,
+            d_model,
+            num_heads,
+            dropout,
+            name="encoder"):
+  inputs = tf.keras.Input(shape=(None,), name="inputs")
+
+	# 패딩 마스크 사용
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+  # 임베딩 레이어
+  embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+  embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+
+  # 포지셔널 인코딩
+  embeddings = PositionalEncoding(vocab_size, d_model)(embeddings)
+
+  outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+  # num_layers만큼 쌓아올린 인코더의 층.
+  for i in range(num_layers):
+    outputs = encoder_layer(
+        units=units,
+        d_model=d_model,
+        num_heads=num_heads,
+        dropout=dropout,
+        name="encoder_layer_{}".format(i),
+    )([outputs, padding_mask])
+
+  return tf.keras.Model(
+      inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+# 디코더 하나의 레이어를 함수로 구현.
+# 이 하나의 레이어 안에는 세 개의 서브 레이어가 존재합니다.
+def decoder_layer(units, d_model, num_heads, dropout, name="decoder_layer"):
+  inputs = tf.keras.Input(shape=(None, d_model), name="inputs")
+  enc_outputs = tf.keras.Input(shape=(None, d_model), name="encoder_outputs")
+  look_ahead_mask = tf.keras.Input(
+      shape=(1, None, None), name="look_ahead_mask")
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+
+  # 첫 번째 서브 레이어 : 멀티 헤드 어텐션 수행 (셀프 어텐션)
+  attention1 = MultiHeadAttention(
+      d_model, num_heads, name="attention_1")(inputs={
+          'query': inputs,
+          'key': inputs,
+          'value': inputs,
+          'mask': look_ahead_mask
+      })
+
+  # 멀티 헤드 어텐션의 결과는 LayerNormalization이라는 훈련을 돕는 테크닉을 수행
+  attention1 = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention1 + inputs)
+
+  # 두 번째 서브 레이어 : 마스크드 멀티 헤드 어텐션 수행 (인코더-디코더 어텐션)
+  attention2 = MultiHeadAttention(
+      d_model, num_heads, name="attention_2")(inputs={
+          'query': attention1,
+          'key': enc_outputs,
+          'value': enc_outputs,
+          'mask': padding_mask
+      })
+
+  # 마스크드 멀티 헤드 어텐션의 결과는
+  # Dropout과 LayerNormalization이라는 훈련을 돕는 테크닉을 수행
+  attention2 = tf.keras.layers.Dropout(rate=dropout)(attention2)
+  attention2 = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention2 + attention1)
+
+  # 세 번째 서브 레이어 : 2개의 완전연결층
+  outputs = tf.keras.layers.Dense(units=units, activation='relu')(attention2)
+  outputs = tf.keras.layers.Dense(units=d_model)(outputs)
+
+  # 완전연결층의 결과는 Dropout과 LayerNormalization 수행
+  outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+  outputs = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(outputs + attention2)
+
+  return tf.keras.Model(
+      inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask],
+      outputs=outputs,
+      name=name)
+
+def decoder(vocab_size,
+            num_layers,
+            units,
+            d_model,
+            num_heads,
+            dropout,
+            name='decoder'):
+  inputs = tf.keras.Input(shape=(None,), name='inputs')
+  enc_outputs = tf.keras.Input(shape=(None, d_model), name='encoder_outputs')
+  look_ahead_mask = tf.keras.Input(
+      shape=(1, None, None), name='look_ahead_mask')
+
+	# 패딩 마스크
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+  
+	# 임베딩 레이어
+  embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+  embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+
+	# 포지셔널 인코딩
+  embeddings = PositionalEncoding(vocab_size, d_model)(embeddings)
+
+	# Dropout이라는 훈련을 돕는 테크닉을 수행
+  outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+  for i in range(num_layers):
+    outputs = decoder_layer(
+        units=units,
+        d_model=d_model,
+        num_heads=num_heads,
+        dropout=dropout,
+        name='decoder_layer_{}'.format(i),
+    )(inputs=[outputs, enc_outputs, look_ahead_mask, padding_mask])
+
+  return tf.keras.Model(
+      inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask],
+      outputs=outputs,
+      name=name)
+
+path_to_zip = tf.keras.utils.get_file(
+    'cornell_movie_dialogs.zip',
+    origin='http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip',
+    extract=True)
+
+path_to_dataset = os.path.join(
+    os.path.dirname(path_to_zip), "cornell movie-dialogs corpus")
+
+path_to_movie_lines = os.path.join(path_to_dataset, 'movie_lines.txt')
+path_to_movie_conversations = os.path.join(path_to_dataset,'movie_conversations.txt')
+
+# 전처리 함수
+MAX_SAMPLES = 50000
+
+def preprocess_sentence(sentence):
+  sentence = sentence.lower().strip()
+
+  # 단어와 구두점(punctuation) 사이의 거리를 만듭니다.
+  # 예를 들어서 "I am a student." => "I am a student ."와 같이
+  # student와 온점 사이에 거리를 만듭니다.
+  sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+  sentence = re.sub(r'[" "]+', " ", sentence)
+
+  # (a-z, A-Z, ".", "?", "!", ",")를 제외한 모든 문자를 공백인 ' '로 대체합니다.
+  sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
+  sentence = sentence.strip()
+  return sentence
+
+# 질문과 답변의 쌍인 데이터셋을 구성하기 위한 데이터 로드 함수
+def load_conversations():
+  id2line = {}
+  with open(path_to_movie_lines, errors='ignore') as file:
+    lines = file.readlines()
+  for line in lines:
+    parts = line.replace('\n', '').split(' +++$+++ ')
+    id2line[parts[0]] = parts[4]
+
+  inputs, outputs = [], []
+  with open(path_to_movie_conversations, 'r') as file:
+    lines = file.readlines()
+
+  for line in lines:
+    parts = line.replace('\n', '').split(' +++$+++ ')
+    conversation = [line[1:-1] for line in parts[3][1:-1].split(', ')]
+
+    for i in range(len(conversation) - 1):
+			# 전처리 함수를 질문에 해당되는 inputs와 답변에 해당되는 outputs에 적용.
+      inputs.append(preprocess_sentence(id2line[conversation[i]]))
+      outputs.append(preprocess_sentence(id2line[conversation[i + 1]]))
+
+      if len(inputs) >= MAX_SAMPLES:
+        return inputs, outputs
+  return inputs, outputs
+
+
+# 단어장 만들기
+questions, answers = load_conversations()
+tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(questions + answers, target_vocab_size=2**13)
+
+# 시작 토큰가 끝 토큰에 고유 정수 부여
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+
+# 단어장 크기 산정
+VOCAB_SIZE = tokenizer.vocab_size + 2
+
+# 토크나이저 함수
+MAX_LENGTH = 40
+
+def tokenize_and_filter(inputs, outputs):
+  tokenized_inputs, tokenized_outputs = [], []
+  
+  for (sentence1, sentence2) in zip(inputs, outputs):
+    # 정수 인코딩 과정에서 시작 토큰과 종료 토큰을 추가
+    sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+
+    # 최대 길이 40 이하인 경우에만 데이터셋으로 허용
+    if len(sentence1) <= MAX_LENGTH and len(sentence2) <= MAX_LENGTH:
+      tokenized_inputs.append(sentence1)
+      tokenized_outputs.append(sentence2)
+  
+  # 최대 길이 40으로 모든 데이터셋을 패딩
+  tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_inputs, maxlen=MAX_LENGTH, padding='post')
+  tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_outputs, maxlen=MAX_LENGTH, padding='post')
+  
+  return tokenized_inputs, tokenized_outputs
+
+questions, answers = tokenize_and_filter(questions, answers)
+
+# 교사 강요
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# 디코더는 이전의 target을 다음의 input으로 사용합니다.
+# 이에 따라 outputs에서는 START_TOKEN을 제거하겠습니다.
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': questions,
+        'dec_inputs': answers[:, :-1]
+    },
+    {
+        'outputs': answers[:, 1:]
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+# 모델 정의 및 학습하기 
+def transformer(vocab_size,
+                num_layers,
+                units,
+                d_model,
+                num_heads,
+                dropout,
+                name="transformer"):
+  inputs = tf.keras.Input(shape=(None,), name="inputs")
+  dec_inputs = tf.keras.Input(shape=(None,), name="dec_inputs")
+
+	# 인코더에서 패딩을 위한 마스크
+  enc_padding_mask = tf.keras.layers.Lambda(
+      create_padding_mask, output_shape=(1, 1, None),
+      name='enc_padding_mask')(inputs)
+
+  # 디코더에서 미래의 토큰을 마스크 하기 위해서 사용합니다.
+  # 내부적으로 패딩 마스크도 포함되어져 있습니다.
+  look_ahead_mask = tf.keras.layers.Lambda(
+      create_look_ahead_mask,
+      output_shape=(1, None, None),
+      name='look_ahead_mask')(dec_inputs)
+
+  # 두 번째 어텐션 블록에서 인코더의 벡터들을 마스킹
+  # 디코더에서 패딩을 위한 마스크
+  dec_padding_mask = tf.keras.layers.Lambda(
+      create_padding_mask, output_shape=(1, 1, None),
+      name='dec_padding_mask')(inputs)
+
+  # 인코더
+  enc_outputs = encoder(
+      vocab_size=vocab_size,
+      num_layers=num_layers,
+      units=units,
+      d_model=d_model,
+      num_heads=num_heads,
+      dropout=dropout,
+  )(inputs=[inputs, enc_padding_mask])
+
+  # 디코더
+  dec_outputs = decoder(
+      vocab_size=vocab_size,
+      num_layers=num_layers,
+      units=units,
+      d_model=d_model,
+      num_heads=num_heads,
+      dropout=dropout,
+  )(inputs=[dec_inputs, enc_outputs, look_ahead_mask, dec_padding_mask])
+
+  # 완전연결층
+  outputs = tf.keras.layers.Dense(units=vocab_size, name="outputs")(dec_outputs)
+
+  return tf.keras.Model(inputs=[inputs, dec_inputs], outputs=outputs, name=name)
+
+# 모델 생성
+tf.keras.backend.clear_session()
+
+# 하이퍼파라미터
+NUM_LAYERS = 2 # 인코더와 디코더의 층의 개수
+D_MODEL = 256 # 인코더와 디코더 내부의 입, 출력의 고정 차원
+NUM_HEADS = 8 # 멀티 헤드 어텐션에서의 헤드 수 
+UNITS = 512 # 피드 포워드 신경망의 은닉층의 크기
+DROPOUT = 0.1 # 드롭아웃의 비율
+
+model = transformer(
+     vocab_size=VOCAB_SIZE,
+     num_layers=NUM_LAYERS,
+     units=UNITS,
+     d_model=D_MODEL,
+     num_heads=NUM_HEADS,
+     dropout=DROPOUT)
+
+model.summary()
+
+# 손실함수
+def loss_function(y_true, y_pred):
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  
+  loss = tf.keras.losses.SparseCategoricalCrossentropy(
+      from_logits=True, reduction='none')(y_true, y_pred)
+
+  mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+  loss = tf.multiply(loss, mask)
+
+  return tf.reduce_mean(loss)
+
+#커스텀된 학습률
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+  def __init__(self, d_model, warmup_steps=4000):
+    super(CustomSchedule, self).__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps**-1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+#sample_learning_rate = CustomSchedule(d_model=128)
+
+#plt.plot(sample_learning_rate(tf.range(200000, dtype=tf.float32)))
+#plt.ylabel("Learning Rate")
+#plt.xlabel("Train Step")
+
+#모델 컴파일
+learning_rate = CustomSchedule(D_MODEL)
+
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate = 0.001, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+def accuracy(y_true, y_pred):
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+
+#훈련하기
+EPOCHS = 20
+history = model.fit(dataset, epochs=EPOCHS, verbose=1)
+
+#모델 저장
+print('saving model...')
+model_path = os.getenv("HOME")+'/chatbot_v0.1_file'
+model.save(model_path)
+
+#모델 로딩
+print('Loading model...')
+model = keras.models.load_model(model_path, custom_objects = {"loss_function": loss_function})
+
+def decoder_inference(sentence):
+  sentence = preprocess_sentence(sentence)
+
+  # 입력된 문장을 정수 인코딩 후, 시작 토큰과 종료 토큰을 앞뒤로 추가.
+  # ex) Where have you been? → [[8331   86   30    5 1059    7 8332]]
+  sentence = tf.expand_dims(
+      START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+  # 디코더의 현재까지의 예측한 출력 시퀀스가 지속적으로 저장되는 변수.
+  # 처음에는 예측한 내용이 없음으로 시작 토큰만 별도 저장. ex) 8331
+  output_sequence = tf.expand_dims(START_TOKEN, 0)
+
+  # 디코더의 인퍼런스 단계
+  for i in range(MAX_LENGTH):
+    # 디코더는 최대 MAX_LENGTH의 길이만큼 다음 단어 예측을 반복합니다.
+    predictions = model(inputs=[sentence, output_sequence], training=False)
+    predictions = predictions[:, -1:, :]
+
+    # 현재 예측한 단어의 정수
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    # 만약 현재 예측한 단어가 종료 토큰이라면 for문을 종료
+    if tf.equal(predicted_id, END_TOKEN[0]):
+      break
+
+    # 예측한 단어들은 지속적으로 output_sequence에 추가됩니다.
+    # 이 output_sequence는 다시 디코더의 입력이 됩니다.
+    output_sequence = tf.concat([output_sequence, predicted_id], axis=-1)
+
+  return tf.squeeze(output_sequence, axis=0)
+
+def sentence_generation(sentence):
+  # 입력 문장에 대해서 디코더를 동작 시켜 예측된 정수 시퀀스를 리턴받습니다.
+    prediction = decoder_inference(sentence)
+
+  # 정수 시퀀스를 다시 텍스트 시퀀스로 변환합니다.
+    predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
+
+    print('입력 : {}'.format(sentence))
+    print('출력 : {}'.format(predicted_sentence))
+
+    return predicted_sentence
+
+#sentence_generation('Where have you been?')
+sentence_generation(input())
