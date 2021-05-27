fd8ed0fffbd38a1e58459e18a999602c6583784a
Jeongeun-Kwak <wjddms960921@gmail.com>
2021-05-27 22:04:28 +0900

    Delete tts.py

--- tts.py
+++
@@ -1,6 +0,0 @@
-curl -v -X POST "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize" \
--H "Content-Type: application/xml" \
--H "Authorization: KakaoAK f086b91dd42878e96e93ebee3d0195fe" \
--d '<speak> 그는 그렇게 말했습니다. 
-<voice name="MAN_DIALOG_BRIGHT">잘 지냈어? 나도 잘 지냈어.</voice> 
-<voice name="WOMAN_DIALOG_BRIGHT" speechStyle="SS_ALT_FAST_1">금요일이 좋아요.</voice> </speak>' > result.mp3
